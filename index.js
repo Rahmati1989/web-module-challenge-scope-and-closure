e6090546a3e8c6f63676c5ad2aac87c509fa3b9e
Rafiullah Rahmati <rafiullahrahmati@Rafiullahs-MacBook-Pro.local>
2020-09-02 17:44:13 -0700

    Completed task1 and task2 in index.js file

--- index.js
+++ index.js
@@ -27,12 +27,26 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
- * 
+ * Answer:
+ * counter1 uses the private variable `count` to keep track of the score, while
+  counter2 uses a variable `count` described outside the function.
+
  * 2. Which of the two uses a closure? How can you tell?
- * 
+ * Answer:
+ * counter1 makes use of closer through the child function `counter()`
+  using the variable `count` from the parent function. While counter2
+  does something similar, function counter2 is a parent, not a child,
+  and the variable `count` is a global variable. 
+
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
-*/
+ *  Answer:
+ * counter1/counterMaker is better when you need to track several
+    independent counters. The problem with this approach is that you
+    cannot access the count without incrementing the counter. counter2
+    addresses this issue, since count is defined as a global
+    variable. However, a new count variable and a new function must be
+    defined for every score you need to keep track of.
+    */
 
 // counter1 code
 function counterMaker() {
@@ -56,12 +70,12 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+function inning(){
 
-    /*Code Here*/
+  return Math.floor(Math.round()*3)
 
 }
-
+inning();
 /* Task 3: finalScore()
 
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
